7f27b1f5ec01529f73c0411455bb1ab90b461763
Jason Hegarty <jason@jheg.co.uk>
2024-09-27 16:15:08 +0100

    uploading initial files for PY110 lesson 1

---
+++ lesson_1/lists.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4]
+for index in range(len(numbers)):
+    numbers[index] += 1
+
+print(numbers)
+#  numbers[4] += 6 IndexError: list index out of range
---
+++ lesson_1/tuples.py
@@ -0,0 +1,5 @@
+fruits = ("apple", "orange", "banana")
+print(fruits[0]) # apple
+print(fruits[-1]) # banana
+# print(fruits[-4]) # IndexError: tuple index out of range
+# fruits[0] = "cherry" # TypeError: 'tuple' object does not support item assignment
