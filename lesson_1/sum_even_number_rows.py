ff5f3dcbee87fab650b0da1bf7f57e56b35c100b
Jason Hegarty <jason@jheg.co.uk>
2024-10-10 08:47:33 +0100

    sum even numbers file

--- lesson_1/sum_even_number_rows.py
+++ lesson_1/sum_even_number_rows.py
@@ -26,78 +26,61 @@ Rules:
     - 22, 24, 26, 28, 30
 
 Algorithm: 
-- Create a create_rows function that takes a single argument, row_number
-- Declare a rows variable and initialise it to an empty list
-- Declare a counter variable to 1
-- Declare a current_iteration to 2
+1 Create a rows list to hold all our rows
+2 Create a row list and add it to the rows list 
+3 Repeat adding rows until all rows have been created
+    - All the rows have been created when the length of the 'rows' list is equal to the input
+4 Sum the final row
+5 Return the sum of the last row
 
-HERE
-- For each number from counter to row_number   
-    - Add an empty list to rows
-    -   for number in range(counter: row_number)
-        - Append current_iteration
-        - Increment current_iteration by 2
+    - Problem: 2: Create a row
+    - Rules: 
+        - The row is a list
+        - Row contains integers
+        - The integers are consecutive even numbers
+        - The integers in each row form part of an overal sequence
+        - Each row has a different length
 
+        Input: 
+            - Length of the row 
+            - The starting integer 
+        Output: The row itself  --> [8, 10, 12]
+
+    Examples: 
+    - Start: 2, length: 1 --> [2]
+    - Start: 4, length: 2 --> [4, 6]
+    - Start: 8, length: 3 --> [8, 10, 12]
+
+    - Data structures: list
+
+    Algorithm:
+        1 Create an empty row containing the integers
+        2 Add the starting integer   
+        3 Increment the starting integer by 2 to get the next integer in the sequence
+        4 Repeat steps 2 & 3 until row has reached the correct length
+        5 Return the 'row' list
 
 END
 """
 
-## The PEDAC Process
-
-## Sum Even Number Rows
-# Imagine a sequence of consecutive even integers beginning with two. The integers
-# are grouped in rows, with the first row containing one integer, the second row
-# two integers, the third row three integers, and so on. Given an integer 
-# representing the number of a particular row, return an integer representing the
-# sum of all the integers in that row. 
-
-## P: Understand the Problem
-#   - Input: Integer representing the row number
-#   - Output: Integer representing the sum of all numbers in that row
-#   - Explicit 
-#       - Sequence of numbers
-#       - Sequence begins with 2
-#       - Integers are consecutive
-#       - Integers are even
-#       - Sequence is grouped into rows
-#       - Each row is incrementally larger than the last 1, 2, 3, ...
-
-#   - Implicit 
-#       - Row 1 has 1 element, Row 2 has 2 elements, Row 3 has 3 elements, etc
-
-# - Establish the rules define the boundaries of the problem
-# - Restate the problem in your own words
-# - Identify problem requirements: 
-# Identify Inputs and Outputs 
-# Ask questions / identify unclear information
-# Spend enough time here. Don't rush.
-
-## General Example: 
-# Given a string 
- 
-## E: 
-## D:
-## A: 
-## C:
-
-
-def create_rows(row_number):
+def sum_even_number_row(row_number):
     rows = []
-    for num in range(row_number):
-        rows.append([])
-    print(rows)
-    
-    value = 2
-    for list in rows:
-        count = 0
-        while count < row_number:
-            list.append(value)
-            count += 1
-            value += 2
-    
-    print(f'{rows = }')
+    start_integer = 2
+    for row_length in range(1, row_number + 1):
+        row = create_row(start_integer, row_length)
+        rows.append(row)
+        start_integer = row[-1] + 2
 
-create_rows(1) # 2 -> [2]
-create_rows(2) # 10 -> [4, 6]
-create_rows(3) # 30 -> [8, 10, 12]
-create_rows(5) # 130 -> [22, 24, 26, 28, 30]
+    return sum(rows[-1])
+
+def create_row(start_integer, row_length):
+    row = []
+    current_integer = start_integer
+    while len(row) < row_length:
+        row.append(current_integer)
+        current_integer += 2
+    return row
+
+print(sum_even_number_row(1) == 2)
+print(sum_even_number_row(2) == 10)
+print(sum_even_number_row(4) == 68)
