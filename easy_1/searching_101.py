1c5e98ddf7f1968867f5f05bafdcc7c27376022b
Jason Hegarty <jason@jheg.co.uk>
2024-10-20 08:05:23 +0100

    initial commit for practice problems

---
+++ easy_1/searching_101.py
@@ -0,0 +1,36 @@
+# Searching 101
+# Write a program that solicits six (6) numbers from the user and prints a 
+# message that describes whether the sixth number appears among the first five.
+
+# Examples:
+# Enter the 1st number: 25
+# Enter the 2nd number: 15
+# Enter the 3rd number: 20
+# Enter the 4th number: 17
+# Enter the 5th number: 23
+# Enter the last number: 17
+
+# 17 is in 25,15,20,17,23.
+
+# Enter the 1st number: 25
+# Enter the 2nd number: 15
+# Enter the 3rd number: 20
+# Enter the 4th number: 17
+# Enter the 5th number: 23
+# Enter the last number: 18
+
+# 18 isn't in 25,15,20,17,23.
+
+"""
+Given six inputs from a user, expected to be whole numbers, check to see if the 
+last number given appears in the first five numbers. If it is, then return a  
+confirmation that number is in the list of five numbers. If it is not then 
+confirm it is not in the list of five numbers. 
+
+Input: 6 string inputs from a user
+Output: string
+
+Rules: 
+Explicit: 
+- 
+"""
